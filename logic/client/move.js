d3f2d350d249499b14d1189c4fe1a05b4b373794
MxTec.inc@gmail.com <MxTec.inc@gmail.com>
2022-09-05 21:25:07 -0400

    Fixed radians to pos formula

--- logic/client/move.js
+++ logic/client/move.js
@@ -5,7 +5,7 @@ const { getPos, setPos } = require(config.LOGIC + "/helpers/pos_db.js");
 var lastMove = {};
 
 const circleXY = (s, theta) => {
-    theta = (theta - 90) * Math.PI / 180;
+    //theta = (theta - 90) * Math.PI / 180;
     return {
         x: s * Math.cos(theta.toFixed(5)),
         y: -s * Math.sin(theta.toFixed(5))
@@ -15,12 +15,12 @@ const circleXY = (s, theta) => {
 const move = async (io, socket, id) => {
     socket.on("move", (data) => {
         const atime = new Date().getTime();
-        if ((lastMove[id] ? lastMove[id] : 0) + 33 <= atime && data.x && data.y && data.a) {
+        if ((lastMove[id] ? lastMove[id] : 0) + 5 <= atime && data.a) {
             lastMove[id] = atime;
             let { map , x , y } = getPos(id);
             const xy = circleXY(3 , data.a);
-            x += -xy.x;
-            y += -xy.y;
+            x += xy.x;
+            y += xy.y;
             setPos(id, map, x , y , data.a);
             setMove(id, map, x , y , data.a);
         }
